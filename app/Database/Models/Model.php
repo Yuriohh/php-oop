3d93da450ec006f001118e2d910927501905dce4
Yuri Santos de Lima <yuri.y.u.r.i@hotmail.com>
2024-01-27 19:03:54 -0300

    feat: add abstract class Model

---
+++ app/Database/Models/Model.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Database\Models;
+
+use App\Database\Connection;
+use App\Database\Filters;
+use PDO;
+use PDOException;
+
+abstract class Model
+{
+    private string $fields = '*';
+    protected string $table = '';
+    private string $filters = '';
+
+    public function setFields(string $fields)
+    {
+        $this->fields = $fields;
+    }
+
+    public function setFilters(Filters $filters)
+    {
+        $this->filters = $filters->dump();
+    }
+
+    public function fetchAll()
+    {
+        try {
+            $sql = "select {$this->fields} from {$this->table} {$this->filters}";
+
+            $connection = Connection::connect();
+
+            $query = $connection->query($sql);
+
+            return $query->fetchAll(PDO::FETCH_CLASS, get_called_class());
+        } catch(PDOException $e) {
+            dd($e->getMessage());
+        }
+    }
+
+    public function findBy(string $field, string $value)
+    {
+        try {
+            $sql = (!$this->filters) ? 
+                "select {$this->fields} from {$this->table} where {$field} = :{$field}":
+                "select {$this->fields} from {$this->table} {$this->filters}";
+
+            $connection = Connection::connect();
+
+            $prepare = $connection->prepare($sql);
+            $prepare->execute(!$this->filters ? [$field => $value] : []);
+
+            return $prepare->fetchObject(get_called_class());
+        } catch(PDOException $e) {
+            dd($e->getMessage());
+        }
+    }
+
+    public function first(string $field = 'id', string $order = 'asc')
+    {
+        try{
+            $sql = "select {$this->fields} from {$this->table} order by {$field} {$order} limit 1";
+
+            $connection = Connection::connect();
+
+            $query = $connection->query($sql);
+
+            return $query->fetchObject(get_called_class());
+        } catch(PDOException $e) {
+            dd($e->getMessage());
+        }
+    }
+
+    public function delete(string $field = '', string|int $value = ''): bool
+    {
+        try {
+            $sql = (empty($this->filters)) ?
+            "delete from {$this->table} where {$field} = :{$field}" :
+            "delete from {$this->table} {$this->filters}";
+
+            $connection = Connection::connect();
+
+            $prepare = $connection->prepare($sql);
+            return $prepare->execute(empty($this->filters) ? [$field => $value] : []);
+
+        } catch(PDOException $e) {
+            dd($e->getMessage());
+        }
+    }
+
+    public function count()
+    {
+        try{
+            $sql = "select {$this->fields} from {$this->table} {$this->filters}";
+
+            $connection = Connection::connect();
+
+            $query = $connection->query($sql);
+
+            return $query->rowCount();
+        } catch(PDOException $e) {
+            dd($e->getMessage());
+        }
+    }
+}
