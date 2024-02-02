0598973dc6d970f64685b07c6568dd1320fd9df0
Yuri Santos de Lima <yuri.y.u.r.i@hotmail.com>
2024-02-02 08:38:50 -0300

    feat: add flash messages in user form

--- app/Views/user.php
+++ app/Views/user.php
@@ -1,12 +1,22 @@
 <?php $this->layout('master', ['title' => 'User Profile']) ?>
 
 <h1>User Profile</h1>
+<?= flash('created') ?>
 
 <form action="/user/update/12" method="post">
 
+    <?= csrf() ?>
+
+    <?= flash('firstName') ?>
     <input type="text" name="firstName" value="Yuriohh">
+
+    <?= flash('lastName') ?>
     <input type="text" name="lastName" value="Lima">
+
+    <?= flash('email') ?>
     <input type="text" name="email" value="yuriohh@teste.com">
+
+    <?= flash('password') ?>
     <input type="password" name="password" value="987654321">
 
     <button type="submit">Atualizar</button>
