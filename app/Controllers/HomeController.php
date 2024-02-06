8cbfd2806ebd812d81aeae8d4e41f027b8fe8cb6
Yuri Santos de Lima <yuri.y.u.r.i@hotmail.com>
2024-02-06 08:18:07 -0300

    feat: add HomeController

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,10 +2,30 @@
 
 namespace App\Controllers;
 
+use App\Database\Filters;
+use App\Database\Models\User;
+use App\Database\Pagination;
+
 class HomeController extends Controller
 {
     public function index()
     {
-        return $this->view('home');
+        $filters = new Filters();
+        //$filters->where('id', '>', 1);
+
+        $pagination = new Pagination();
+        $pagination->setItemsPerPage(1);
+
+        $user = new User();
+        $user->setFields('id, firstname, lastname');
+        $user->setFilters($filters);
+        $user->setPagination($pagination);
+        $usersFound = $user->fetchAll();
+
+        return $this->view('home', [
+            'title' => 'home',
+            'usersFound' => $usersFound,
+            'pagination' => $pagination,
+        ]);
     }
 }
