91d3457e2cc3635f1117e5087efc01d6551aebb9
Yuri Santos de Lima <yuri.y.u.r.i@hotmail.com>
2024-02-06 08:14:09 -0300

    chore: add dotenv package

--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Core\Router;
+use Dotenv\Dotenv;
 
 require '../vendor/autoload.php';
 
 session_start();
+
+$dotenv = Dotenv::createImmutable('../');
+$dotenv->load();
 
 Router::run();
